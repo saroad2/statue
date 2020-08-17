2563b089823b1bd1d56ee611eb7b4106287216a3
Sagi Shadur <saroad2@gmail.com>
2020-08-17 15:52:20 +0300

    remove statue commands dependencies

--- tests/command/conftest.py
+++ tests/command/conftest.py
@@ -2,12 +2,19 @@ import pytest
 import os
 
 
+from statue.command import Command
+
 ENVIRON = dict(s=2, d=5, g=8)
 
 
 @pytest.fixture
 def subprocess_mock(mocker):
     return mocker.patch("subprocess.run")
+
+
+@pytest.fixture
+def available_packages_mock(mocker):
+    return mocker.patch.object(Command, "available_packages")
 
 
 @pytest.fixture
