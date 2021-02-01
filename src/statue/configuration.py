b276a7f399bcb1991897baec331969fb71fa9147
Sagi Shadur <saroad2@gmail.com>
2021-02-01 18:46:55 +0200

    Fixed documentation of configuration.py

--- src/statue/configuration.py
+++ src/statue/configuration.py
@@ -38,14 +38,26 @@ class Configuration:
 
     @classmethod
     def configuration_path(cls, directory: Union[Path, str]) -> Path:
-        """Get default path of configuration file in directory."""
+        """
+        Get default path of configuration file in directory.
+
+        :param directory: Directory in which the configuration path is supposed to be
+        :type directory: Path or str
+        :return: Configuration path location
+        :rtype: Path
+        """
         if isinstance(directory, str):
             directory = Path(directory)
         return directory / "statue.toml"
 
     @classmethod
     def default_configuration(cls) -> Optional[MutableMapping[str, Any]]:
-        """Getter of default configuration."""
+        """
+        Default configuration path.
+
+        :return: Default configuration path location
+        :rtype: Path
+        """
         if cls.__default_configuration is None:
             cls.__load_default_configuration()
         return deepcopy(cls.__default_configuration)
@@ -54,12 +66,25 @@ class Configuration:
     def set_default_configuration(
         cls, default_configuration: Optional[MutableMapping[str, Any]]
     ) -> None:
-        """Setter of default configuration."""
+        """
+        Setter of default configuration.
+
+        :param default_configuration: Configuration to be saved
+        :type default_configuration: None or mutable mapping
+        """
         cls.__default_configuration = default_configuration
 
     @classmethod
     def statue_configuration(cls) -> MutableMapping[str, Any]:
-        """Getter of general statue configuration."""
+        """
+        Getter of general statue configuration.
+
+        If not set, take default configuration instead.
+
+        :return: Configuration dictionary of Statue
+        :rtype: MutableMapping[str, Any]
+        :raises EmptyConfiguration: Raised if no configuration was set.
+        """
         if cls.__statue_configuration is not None:
             return deepcopy(cls.__statue_configuration)
         default_configuration = cls.default_configuration()
@@ -71,17 +96,32 @@ class Configuration:
     def set_statue_configuration(
         cls, statue_configuration: Optional[MutableMapping[str, Any]]
     ) -> None:
-        """Setter of general statue configuration."""
+        """
+        Setter of general statue configuration.
+
+        :param statue_configuration: Configuration to be saved
+        :type statue_configuration: None or mutable mapping
+        """
         cls.__statue_configuration = statue_configuration
 
     @classmethod
     def commands_configuration(cls) -> Optional[MutableMapping[str, Any]]:
-        """Getter of the commands configuration."""
+        """
+        Getter of the commands configuration.
+
+        :return: Commands configuration dictionary
+        :rtype: MutableMapping[str, Any]
+        """
         return cls.statue_configuration().get(COMMANDS, None)
 
     @classmethod
     def commands_names_list(cls) -> List[str]:
-        """Getter of the commands list."""
+        """
+        List of all available commands.
+
+        :return: Available commands list
+        :rtype: List[str]
+        """
         commands_configuration = cls.commands_configuration()
         if commands_configuration is None:
             return []
@@ -97,8 +137,8 @@ class Configuration:
         :param command_name: Name of the desired command.
         :type command_name: str
         :return: configuration dictionary.
-        :raises: raise :Class:`MissingConfiguration` if no contexts configuration was
-        set.
+        :rtype: None or mutable mapping
+        :raises MissingConfiguration: raised when no contexts configuration was set.
         """
         commands_configuration = cls.commands_configuration()
         if commands_configuration is None:
@@ -109,7 +149,13 @@ class Configuration:
     def sources_configuration(
         cls,
     ) -> MutableMapping[Path, MutableMapping[str, Any]]:
-        """Getter of the sources configuration."""
+        """
+        Getter of the sources configuration.
+
+        :return: Sources configuration dictionary
+        :rtype: MutableMapping[str, Any]
+        :raises MissingConfiguration: raised if no sources configuration was set.
+        """
         sources_configuration: Optional[
             MutableMapping[Path, MutableMapping[str, Any]]
         ] = cls.statue_configuration().get(SOURCES, None)
@@ -119,7 +165,12 @@ class Configuration:
 
     @classmethod
     def sources_list(cls) -> List[Path]:
-        """Getter of the commands list."""
+        """
+        List of sources to run statue over, as specified in the configuration file.
+
+        :return: Available sources list
+        :rtype: List[Path]
+        """
         return list(cls.sources_configuration().keys())
 
     @classmethod
@@ -132,8 +183,7 @@ class Configuration:
         :param source: Name of the desired source.
         :type source: str
         :return: configuration dictionary.
-        :raises: raise :Class:`MissingConfiguration` if no contexts configuration was
-        set.
+        :rtype: None or mutable mapping
         """
         sources_configuration = cls.sources_configuration()
         if not isinstance(source, Path):
@@ -148,12 +198,22 @@ class Configuration:
 
     @classmethod
     def contexts_map(cls) -> Optional[Dict[str, Context]]:
-        """Getter of the contexts configuration."""
+        """
+        Contexts configuration.
+
+        :return: Sources configuration dictionary
+        :rtype: None or dictionary
+        """
         return cls.statue_configuration().get(CONTEXTS, None)
 
     @classmethod
     def contexts_list(cls) -> List[Context]:
-        """Getter of the contexts configuration."""
+        """
+        List of all available contexts.
+
+        :return: Available contexts list
+        :rtype: List[Context]
+        """
         contexts_map = cls.contexts_map()
         if contexts_map is None:
             return []
@@ -167,8 +227,9 @@ class Configuration:
         :param context_identifier: Name or alias of the desired context.
         :type context_identifier: str
         :return: configuration dictionary.
-        :raises: raise :Class:`MissingConfiguration` if no contexts configuration was
-        set.
+        :rtype: Context
+        :raises MissingConfiguration: raised if no contexts configuration was set.
+        :raises UnknownContext: raised if context was not found.
         """
         contexts_configuration = cls.contexts_map()
         if contexts_configuration is None:
@@ -189,12 +250,16 @@ class Configuration:
         deny_list: Optional[List[str]] = None,
     ) -> List[Command]:
         """
-        Read commands from a settings file.
+        Read commands with given constraints.
 
-        :param contexts: List of str. a list of contexts to choose commands from.
+        :param contexts: List of contexts to choose commands from.
+        :type contexts: None or List[str]
         :param allow_list: List of allowed commands. If None, take all commands
+        :type allow_list: None or List[str]
         :param deny_list: List of denied commands. If None, take all commands
-        :return: a list of :class:`Command`
+        :type deny_list: None or List[str]
+        :return: List of commands according to constraints
+        :rtype: List[Command]
         """
         commands = []
         contexts = [] if contexts is None else contexts
@@ -221,16 +286,21 @@ class Configuration:
         deny_list: Optional[List[str]] = None,
     ) -> Command:
         """
-        Read command from a settings file.
+        Read command with given constraints.
 
-        :param command_name: the name of the command to read.
-        :param contexts: List of str. a list of contexts to choose commands from.
+        :param command_name: Name of the command to read
+        :type command_name: str
+        :param contexts: List of contexts to choose commands from.
+        :type contexts: None or List[str]
         :param allow_list: List of allowed commands. If None, take all commands
+        :type allow_list: None or List[str]
         :param deny_list: List of denied commands. If None, take all commands
-        :return: a :class:`Command` instance
-        :raises: :class:`UnknownCommand` if command is missing from settings file.
-        :class:`InvalidCommand` of command doesn't fit the given contexts, allow list
-         and deny list
+        :type deny_list: None or List[str]
+        :return: Command instance with constraints
+        :rtype: Command
+        :raises UnknownCommand: raised if command is missing from settings file.
+        :raises InvalidCommand: raised if command doesn't fit the given contexts,
+            allow list or deny list
         """
         if (
             allow_list is not None
@@ -281,7 +351,8 @@ class Configuration:
         from configuration file.
 
         :param statue_configuration_path: User-defined file path containing
-        repository-specific configurations
+            repository-specific configurations
+        :type statue_configuration_path: None, str or Path
         """
         if statue_configuration_path is None:
             cwd = Path.cwd()
@@ -321,7 +392,10 @@ class Configuration:
         from configuration file.
 
         :param statue_configuration_path: User-defined file path containing
-        repository-specific configurations
+            repository-specific configurations
+        :type statue_configuration_path: Path
+        :return: Configuration mutable map
+        :rtype: None or MutableMapping[str, Any]
         """
         if not statue_configuration_path.exists():
             return None
