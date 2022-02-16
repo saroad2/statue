99d6b53f5277f4bb64126118fe7945f76409cdf0
Sagi Shadur <saroad2@gmail.com>
2022-02-16 16:57:13 +0200

    Make synchronous runner into a class

--- src/statue/runner.py
+++ src/statue/runner.py
@@ -1,38 +1,43 @@
 """Command map runner."""
 import time
-from typing import Callable, Dict, List, Optional
+from typing import Callable, Optional
 
-from statue.command import Command
+from statue.commands_map import CommandsMap
 from statue.evaluation import Evaluation, SourceEvaluation
 
 
-def evaluate_commands_map(
-    commands_map: Dict[str, List[Command]],
-    update_func: Optional[Callable[[Evaluation], None]] = None,
-) -> Evaluation:
-    """
-    Run commands map and return evaluation report.
+class SynchronousEvaluationRunner:  # pylint: disable=too-few-public-methods
+    """Runner class for running command synchronously."""
 
-    :param commands_map: map from input file to list of commands to run on it,
-    :type commands_map: Dict[str, List[Command]],
-    :param update_func: Function to be called before every command is
-        executed. Skip if None
-    :type update_func: Optional[Callable[[Command], None]]
-    :return: Evaluation
-    """
-    evaluation = Evaluation()
-    total_start_time = time.time()
-    for source, commands in commands_map.items():
-        source_start_time = time.time()
-        evaluation[source] = SourceEvaluation()
-        for command in commands:
-            evaluation[source].append(command.execute(source))
-            if update_func is not None:
-                update_func(evaluation)
-        source_end_time = time.time()
-        evaluation[source].source_execution_duration = (
-            source_end_time - source_start_time
-        )
-    total_end_time = time.time()
-    evaluation.total_execution_duration = total_end_time - total_start_time
-    return evaluation
+    @classmethod
+    def evaluate(
+        cls,
+        commands_map: CommandsMap,
+        update_func: Optional[Callable[[Evaluation], None]] = None,
+    ) -> Evaluation:
+        """
+        Run commands map and return evaluation report.
+
+        :param commands_map: map from input file to list of commands to run on it
+        :type commands_map: CommandsMap
+        :param update_func: Function to be called before every command is
+            executed. Skip if None
+        :type update_func: Optional[Callable[[Command], None]]
+        :return: Evaluation
+        """
+        evaluation = Evaluation()
+        total_start_time = time.time()
+        for source, commands in commands_map.items():
+            source_start_time = time.time()
+            evaluation[source] = SourceEvaluation()
+            for command in commands:
+                evaluation[source].append(command.execute(source))
+                if update_func is not None:
+                    update_func(evaluation)
+            source_end_time = time.time()
+            evaluation[source].source_execution_duration = (
+                source_end_time - source_start_time
+            )
+        total_end_time = time.time()
+        evaluation.total_execution_duration = total_end_time - total_start_time
+        return evaluation
