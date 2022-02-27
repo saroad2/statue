e7aa529add58ef9b29c9fb83f09c137166a6c732
Sagi Shadur <saroad2@gmail.com>
2022-02-27 12:56:31 +0200

    Add documentation to contexts repository

--- src/statue/contexts_repository.py
+++ src/statue/contexts_repository.py
@@ -1,4 +1,5 @@
-from typing import Any, MutableMapping
+"""Place for saving all available contexts."""
+from typing import Any, Iterator, MutableMapping
 
 from statue.constants import ALIASES, PARENT
 from statue.context import Context
@@ -6,26 +7,64 @@ from statue.exceptions import InconsistentConfiguration, UnknownContext
 
 
 class ContextsRepository:
+    """Repository class for saving and accessing contexts."""
+
     def __init__(self, *contexts: Context):
+        """
+        Constructor.
+
+        :param contexts: Initial contexts to be saved
+        :type contexts: Context
+        """
         self.contexts_list = list(contexts)
 
     @property
     def contexts_number(self):
+        """Get number of available contexts."""
         return len(self.contexts_list)
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[Context]:
+        """
+        Iterate over all available contexts.
+
+        :return: Contexts iterator
+        :rtype: Iterator[Context]
+        """
         return iter(self.contexts_list)
 
     def add_contexts(self, *contexts: Context):
+        """
+        Add contexts to repository.
+
+        :param contexts: Contexts to be added to the repository
+        :type contexts: Context
+        """
         self.contexts_list.extend(contexts)
 
     def get_context(self, context_name: str) -> Context:
+        """
+        Get context by name or alias.
+
+        :param context_name: Context name or alias to be retrieved
+        :type context_name: str
+        :return: Context with given name
+        :rtype: Context
+        :raises UnknownContext: Raised when context is not found
+        """
         for context in self.contexts_list:
             if context.is_matching(context_name):
                 return context
         raise UnknownContext(context_name=context_name)
 
     def has_context(self, context_name: str) -> bool:
+        """
+        Does context available in repository.
+
+        :param context_name: Context name to be searched
+        :type context_name: str
+        :return: does context exist in repository
+        :rtype: bool
+        """
         try:
             self.get_context(context_name)
         except UnknownContext:
@@ -33,9 +72,18 @@ class ContextsRepository:
         return True
 
     def reset(self):
+        """Clear repository from all contexts."""
         self.contexts_list.clear()
 
     def update_from_config(self, config: MutableMapping[str, Any]):
+        """
+        Update contexts repository from given configuration.
+
+        :param config: Configuration to update repository from
+        :type config: MutableMapping[str, Any]
+        :raises InconsistentConfiguration: Raised when inconsistency is found in
+            configuration.
+        """
         context_names = set(config.keys())
         while len(context_names) != 0:
             context_name = context_names.pop()
@@ -51,6 +99,21 @@ class ContextsRepository:
     def build_context_from_config(
         self, context_name: str, config: MutableMapping[str, Any]
     ) -> Context:
+        """
+        Build context from given configuration.
+
+        If context is not available in configuration, try get it
+        from the repository (if it exists).
+
+        :param context_name: Context to be built
+        :type context_name: str
+        :param config: Configuration to build context from
+        :type config: MutableMapping[str, Any]
+        :return: Built context.
+        :rtype: Context
+        :raises InconsistentConfiguration: Raised when inconsistency is found in
+            configuration.
+        """
         if context_name not in config:
             return self.get_context(context_name)
         context_config = dict(config[context_name])
