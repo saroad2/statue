b87acdf6773ba11b7f1b2ea9fc29e8e27c8d0183
Sagi Shadur <saroad2@gmail.com>
2022-02-28 15:55:48 +0200

    Add new sources repository class

---
+++ src/statue/sources_repository.py
@@ -0,0 +1,96 @@
+"""Place for saving all available sources with default commands filter."""
+from pathlib import Path
+from typing import Any, Dict, List, MutableMapping
+
+from statue.commands_filter import CommandsFilter
+from statue.constants import ALLOW_LIST, CONTEXTS, DENY_LIST
+from statue.contexts_repository import ContextsRepository
+
+
+class SourcesRepository:
+    """
+    Repository class for saving and accessing sources.
+
+    This is done in order to get specific commands filter from each source.
+    """
+
+    def __init__(self):
+        """Initialize repository."""
+        self.sources_filters_map: Dict[Path, CommandsFilter] = {}
+
+    def __len__(self) -> int:
+        """
+        Number of available sources commands filters.
+
+        :return: Number of sources commands filters
+        :rtype: int
+        """
+        return len(self.sources_filters_map)
+
+    def __setitem__(self, key: Path, value: CommandsFilter):
+        """
+        Set a commands filter for a given source.
+
+        :param key: Source to save commands filter for
+        :type key: Path
+        :param value: Command filter for given source
+        :type value: CommandsFilter
+        """
+        self.sources_filters_map[key] = value
+
+    def __getitem__(self, item: Path) -> CommandsFilter:
+        """
+        Get a commands filter for a given source.
+
+        If commands filter is defined for given source, returns default
+        filter.
+
+        :param item: Source to get commands filter for
+        :type item: Path
+        :return: Command filter for given source
+        :rtype: CommandsFilter
+        """
+        for specified_source, commands_filter in self.sources_filters_map.items():
+            try:
+                item.relative_to(specified_source)
+                return commands_filter
+            except ValueError:
+                continue
+        return CommandsFilter()
+
+    @property
+    def sources_list(self) -> List[Path]:
+        """Get list of all available sources."""
+        return list(self.sources_filters_map.keys())
+
+    def reset(self):
+        """Reset sources repository."""
+        self.sources_filters_map.clear()
+
+    def update_from_config(
+        self, config: MutableMapping[str, Any], contexts_repository: ContextsRepository
+    ):
+        """
+        Update sources repository from configuration mapping.
+
+        This is done using an existing contexts repository to link contexts
+        from config to existing context objects
+
+
+        :param config: Configuration to update repository from
+        :type config: MutableMapping[str, Any]
+        :param contexts_repository: Contexts repository to get context objects
+            from
+        :type contexts_repository: ContextsRepository
+
+        """
+        for source, commands_filter_config in config.items():
+            contexts = [
+                contexts_repository[context_name]
+                for context_name in commands_filter_config.get(CONTEXTS, [])
+            ]
+            self.sources_filters_map[Path(source)] = CommandsFilter(
+                contexts=contexts,
+                allowed_commands=commands_filter_config.get(ALLOW_LIST, None),
+                denied_commands=commands_filter_config.get(DENY_LIST, None),
+            )
---
+++ tests/sources_repository/test_sources_repository_update_from_config.py
@@ -0,0 +1,143 @@
+from pathlib import Path
+
+from statue.commands_filter import CommandsFilter
+from statue.constants import ALLOW_LIST, CONTEXTS, DENY_LIST
+from statue.context import Context
+from statue.contexts_repository import ContextsRepository
+from statue.sources_repository import SourcesRepository
+from tests.constants import (
+    COMMAND1,
+    COMMAND2,
+    COMMAND3,
+    COMMAND4,
+    CONTEXT1,
+    CONTEXT2,
+    CONTEXT_HELP_STRING1,
+    CONTEXT_HELP_STRING2,
+    SOURCE1,
+    SOURCE2,
+    SOURCE3,
+)
+
+
+def test_sources_repository_update_from_empty_config():
+    sources_repository = SourcesRepository()
+    commands_filter1, commands_filter2, commands_filter3 = (
+        CommandsFilter(
+            contexts=[Context(name=CONTEXT1, help=CONTEXT_HELP_STRING1)],
+            allowed_commands=[COMMAND1, COMMAND2],
+        ),
+        CommandsFilter(contexts=[Context(name=CONTEXT2, help=CONTEXT_HELP_STRING2)]),
+        CommandsFilter(denied_commands=[COMMAND3, COMMAND4]),
+    )
+    sources_repository[Path(SOURCE1)] = commands_filter1
+    sources_repository[Path(SOURCE2)] = commands_filter2
+    sources_repository[Path(SOURCE3)] = commands_filter3
+
+    sources_repository.update_from_config(
+        config={}, contexts_repository=ContextsRepository()
+    )
+
+    assert len(sources_repository) == 3
+    assert sources_repository[Path(SOURCE1)] == commands_filter1
+    assert sources_repository[Path(SOURCE2)] == commands_filter2
+    assert sources_repository[Path(SOURCE3)] == commands_filter3
+
+
+def test_sources_repository_update_adds_source_with_empty_configuration():
+    sources_repository = SourcesRepository()
+
+    sources_repository.update_from_config(
+        config={SOURCE1: {}}, contexts_repository=ContextsRepository()
+    )
+
+    assert len(sources_repository) == 1
+    assert sources_repository[Path(SOURCE1)] == CommandsFilter()
+
+
+def test_sources_repository_update_adds_source_with_contexts():
+    sources_repository = SourcesRepository()
+    context1, context2 = (
+        Context(name=CONTEXT1, help=CONTEXT_HELP_STRING1),
+        Context(name=CONTEXT2, help=CONTEXT_HELP_STRING2),
+    )
+
+    sources_repository.update_from_config(
+        config={SOURCE1: {CONTEXTS: [CONTEXT1, CONTEXT2]}},
+        contexts_repository=ContextsRepository(context1, context2),
+    )
+
+    assert len(sources_repository) == 1
+    assert sources_repository[Path(SOURCE1)] == CommandsFilter(
+        contexts=[context1, context2]
+    )
+
+
+def test_sources_repository_update_adds_source_with_allowed_commands():
+    sources_repository = SourcesRepository()
+
+    sources_repository.update_from_config(
+        config={SOURCE1: {ALLOW_LIST: [COMMAND1, COMMAND2]}},
+        contexts_repository=ContextsRepository(),
+    )
+
+    assert len(sources_repository) == 1
+    assert sources_repository[Path(SOURCE1)] == CommandsFilter(
+        allowed_commands=[COMMAND1, COMMAND2]
+    )
+
+
+def test_sources_repository_update_adds_source_with_denied_commands():
+    sources_repository = SourcesRepository()
+
+    sources_repository.update_from_config(
+        config={SOURCE1: {DENY_LIST: [COMMAND1, COMMAND2]}},
+        contexts_repository=ContextsRepository(),
+    )
+
+    assert len(sources_repository) == 1
+    assert sources_repository[Path(SOURCE1)] == CommandsFilter(
+        denied_commands=[COMMAND1, COMMAND2]
+    )
+
+
+def test_sources_repository_update_override_source():
+    sources_repository = SourcesRepository()
+    sources_repository[Path(SOURCE1)] = CommandsFilter(
+        contexts=[Context(name=CONTEXT1, help=CONTEXT_HELP_STRING1)],
+        allowed_commands=[COMMAND3],
+    )
+
+    sources_repository.update_from_config(
+        config={SOURCE1: {DENY_LIST: [COMMAND1, COMMAND2]}},
+        contexts_repository=ContextsRepository(),
+    )
+
+    assert len(sources_repository) == 1
+    assert sources_repository[Path(SOURCE1)] == CommandsFilter(
+        denied_commands=[COMMAND1, COMMAND2]
+    )
+
+
+def test_sources_repository_update_adds_multiple_sources():
+    sources_repository = SourcesRepository()
+    context1, context2 = (
+        Context(name=CONTEXT1, help=CONTEXT_HELP_STRING1),
+        Context(name=CONTEXT2, help=CONTEXT_HELP_STRING2),
+    )
+
+    sources_repository.update_from_config(
+        config={
+            SOURCE1: {CONTEXTS: [CONTEXT1], DENY_LIST: [COMMAND1]},
+            SOURCE2: {CONTEXTS: [CONTEXT1, CONTEXT2], ALLOW_LIST: [COMMAND2]},
+        },
+        contexts_repository=ContextsRepository(context1, context2),
+    )
+
+    assert len(sources_repository) == 2
+    assert sources_repository[Path(SOURCE1)] == CommandsFilter(
+        contexts=[context1], denied_commands=[COMMAND1]
+    )
+    assert sources_repository[Path(SOURCE2)] == CommandsFilter(
+        contexts=[context1, context2], allowed_commands=[COMMAND2]
+    )
