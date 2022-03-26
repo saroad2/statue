f7c6543e75ecc8d4e9afd9c363ca8867debfa8e4
Sagi Shadur <saroad2@gmail.com>
2022-03-26 23:28:14 +0300

    Keep order when using `Context.as_dict()`

--- tests/context/test_context_as_dict.py
+++ tests/context/test_context_as_dict.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from pytest_cases import THIS_MODULE, parametrize_with_cases
 
 from statue.constants import ALIASES, ALLOWED_BY_DEFAULT, HELP, PARENT
@@ -14,7 +16,7 @@ from tests.constants import (
 
 def case_simple_context():
     context = Context(name=CONTEXT1, help=CONTEXT_HELP_STRING1)
-    context_dict = {HELP: CONTEXT_HELP_STRING1}
+    context_dict = OrderedDict([(HELP, CONTEXT_HELP_STRING1)])
     return context, context_dict
 
 
@@ -22,20 +24,24 @@ def case_aliased_context():
     context = Context(
         name=CONTEXT1, help=CONTEXT_HELP_STRING1, aliases=[CONTEXT2, CONTEXT3]
     )
-    context_dict = {HELP: CONTEXT_HELP_STRING1, ALIASES: [CONTEXT2, CONTEXT3]}
+    context_dict = OrderedDict(
+        [(HELP, CONTEXT_HELP_STRING1), (ALIASES, [CONTEXT2, CONTEXT3])]
+    )
     return context, context_dict
 
 
 def case_context_with_parent():
     parent = Context(CONTEXT2, help=CONTEXT_HELP_STRING2)
     context = Context(name=CONTEXT1, help=CONTEXT_HELP_STRING1, parent=parent)
-    context_dict = {HELP: CONTEXT_HELP_STRING1, PARENT: CONTEXT2}
+    context_dict = OrderedDict([(HELP, CONTEXT_HELP_STRING1), (PARENT, CONTEXT2)])
     return context, context_dict
 
 
 def case_allowed_by_default_context():
     context = Context(name=CONTEXT1, help=CONTEXT_HELP_STRING1, allowed_by_default=True)
-    context_dict = {HELP: CONTEXT_HELP_STRING1, ALLOWED_BY_DEFAULT: True}
+    context_dict = OrderedDict(
+        [(HELP, CONTEXT_HELP_STRING1), (ALLOWED_BY_DEFAULT, True)]
+    )
     return context, context_dict
 
 
@@ -48,15 +54,23 @@ def case_full_context():
         parent=parent,
         aliases=[CONTEXT3, CONTEXT4],
     )
-    context_dict = {
-        HELP: CONTEXT_HELP_STRING1,
-        ALLOWED_BY_DEFAULT: True,
-        PARENT: CONTEXT2,
-        ALIASES: [CONTEXT3, CONTEXT4],
-    }
+    context_dict = OrderedDict(
+        [
+            (HELP, CONTEXT_HELP_STRING1),
+            (ALIASES, [CONTEXT3, CONTEXT4]),
+            (PARENT, CONTEXT2),
+            (ALLOWED_BY_DEFAULT, True),
+        ]
+    )
     return context, context_dict
 
 
-@parametrize_with_cases(argnames=["context", "context_dict"], cases=THIS_MODULE)
-def test_context_as_dict(context, context_dict):
-    assert context.as_dict() == context_dict
+@parametrize_with_cases(
+    argnames=["context", "expected_context_dict"], cases=THIS_MODULE
+)
+def test_context_as_dict(context, expected_context_dict):
+    context_dict = context.as_dict()
+    assert isinstance(context_dict, OrderedDict)
+    assert list(context_dict.keys()) == list(expected_context_dict.keys())
+    for key, value in expected_context_dict.items():
+        assert context_dict[key] == value
