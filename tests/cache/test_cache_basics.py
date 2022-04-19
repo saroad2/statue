fe0e06fc1b289eba497ca459dfa2883f7cd4fbc7
Sagi Shadur <saroad2@gmail.com>
2022-04-19 23:01:15 +0300

    Split cache basic tests

--- tests/cache/test_cache_basics.py
+++ tests/cache/test_cache_basics.py
@@ -1,6 +1,18 @@
 import random
 
+import pytest
+
 from statue.cache import Cache
+
+EVALUATION_PATH_NAMES = [
+    "evaluation-1000.json",
+    "evaluation-999.json",
+    "evaluation-900.json",
+    "evaluation-889.json",
+    "evaluation-700.json",
+    "evaluation-88.json",
+    "evaluation-70.json",
+]
 
 
 def test_cache_constructor_with_none_root_directory(tmp_path):
@@ -61,13 +73,8 @@ def test_cache_constructor_with_existing_evaluations(tmp_path):
     evaluations_dir = cache_dir / "evaluations"
     evaluations_dir.mkdir(parents=True)
     evaluation_paths = [
-        evaluations_dir / "evaluation-1000.json",
-        evaluations_dir / "evaluation-999.json",
-        evaluations_dir / "evaluation-900.json",
-        evaluations_dir / "evaluation-889.json",
-        evaluations_dir / "evaluation-700.json",
-        evaluations_dir / "evaluation-88.json",
-        evaluations_dir / "evaluation-70.json",
+        evaluations_dir / evaluation_path_name
+        for evaluation_path_name in EVALUATION_PATH_NAMES
     ]
     for evaluation_file in evaluation_paths:
         evaluation_file.touch()
@@ -81,9 +88,27 @@ def test_cache_constructor_with_existing_evaluations(tmp_path):
     assert cache.evaluations_dir.exists()
     assert cache.all_evaluation_paths == evaluation_paths
     assert cache.recent_evaluation_path == evaluation_paths[0]
-    for i, evaluation_path in enumerate(evaluation_paths):
-        assert cache.evaluation_path(i) == evaluation_path
     assert cache.history_size == size
+
+
+@pytest.mark.parametrize(
+    argnames="evaluation_index", argvalues=range(len(EVALUATION_PATH_NAMES))
+)
+def test_cache_get_evaluation_path(tmp_path, evaluation_index):
+    cache_dir = tmp_path / "cache"
+    evaluations_dir = cache_dir / "evaluations"
+    evaluations_dir.mkdir(parents=True)
+    evaluation_paths = [
+        evaluations_dir / evaluation_path_name
+        for evaluation_path_name in EVALUATION_PATH_NAMES
+    ]
+    for evaluation_file in evaluation_paths:
+        evaluation_file.touch()
+
+    size = random.randint(1, 100)
+    cache = Cache(size=size, cache_root_directory=cache_dir)
+
+    assert cache.evaluation_path(evaluation_index) == evaluation_paths[evaluation_index]
 
 
 def test_cache_constructor_with_history_size(tmp_path):
