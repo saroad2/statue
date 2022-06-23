4d267100d983a6300d59e8f9c36ebb6b4f023b6f
Sagi Shadur <saroad2@gmail.com>
2022-06-23 13:52:25 +0300

    Enable and disable cache via command line

--- src/statue/cli/config/config_general.py
+++ src/statue/cli/config/config_general.py
@@ -35,3 +35,27 @@ def set_history_size_cli(size, config):
     configuration.cache.history_size = size
     configuration.to_toml(config)
     click.echo("History size was successfully set!")
+
+
+@config_cli.command("enable-cache")
+@config_path_option
+def enable_cache_cli(config):
+    """Enabled caching in configuration."""
+    if config is None:
+        config = Configuration.configuration_path()
+    configuration = Configuration.from_file(config)
+    configuration.cache.enabled = True
+    configuration.to_toml(config)
+    click.echo("Caching is enabled!")
+
+
+@config_cli.command("disable-cache")
+@config_path_option
+def disable_cache_cli(config):
+    """Disabled caching in configuration."""
+    if config is None:
+        config = Configuration.configuration_path()
+    configuration = Configuration.from_file(config)
+    configuration.cache.enabled = False
+    configuration.to_toml(config)
+    click.echo("Caching is disabled!")
