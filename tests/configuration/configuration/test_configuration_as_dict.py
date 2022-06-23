e4b36b2972164a48b05fc0c3890400cd570fb642
Sagi Shadur <saroad2@gmail.com>
2022-06-23 13:26:19 +0300

    Add `Cache.enabled` field.

--- tests/configuration/configuration/test_configuration_as_dict.py
+++ tests/configuration/configuration/test_configuration_as_dict.py
@@ -5,7 +5,15 @@ import mock
 import pytest
 
 from statue.config.configuration import Configuration
-from statue.constants import COMMANDS, CONTEXTS, GENERAL, HISTORY_SIZE, MODE, SOURCES
+from statue.constants import (
+    CACHE,
+    COMMANDS,
+    CONTEXTS,
+    GENERAL,
+    HISTORY_SIZE,
+    MODE,
+    SOURCES,
+)
 from statue.runner import RunnerMode
 
 
@@ -44,6 +52,30 @@ def test_configuration_as_dict_with_runner_mode(
     assert isinstance(configuration_dict, OrderedDict)
     assert list(configuration_dict.keys()) == [GENERAL, CONTEXTS, COMMANDS, SOURCES]
     assert configuration_dict[GENERAL] == {MODE: mode.name.lower(), HISTORY_SIZE: size}
+    assert configuration_dict[CONTEXTS] == mock_contexts_repository_as_dict.return_value
+    assert configuration_dict[COMMANDS] == mock_commands_repository_as_dict.return_value
+    assert configuration_dict[SOURCES] == mock_sources_repository_as_dict.return_value
+
+
+def test_configuration_as_dict_with_disabled_cache(
+    mock_contexts_repository_as_dict,
+    mock_commands_repository_as_dict,
+    mock_sources_repository_as_dict,
+):
+    size = random.randint(1, 100)
+    cache = mock.Mock()
+    cache.history_size = size
+    cache.enabled = False
+    configuration = Configuration(cache=cache)
+    configuration_dict = configuration.as_dict()
+
+    assert isinstance(configuration_dict, OrderedDict)
+    assert list(configuration_dict.keys()) == [GENERAL, CONTEXTS, COMMANDS, SOURCES]
+    assert configuration_dict[GENERAL] == {
+        MODE: "sync",
+        HISTORY_SIZE: size,
+        CACHE: False,
+    }
     assert configuration_dict[CONTEXTS] == mock_contexts_repository_as_dict.return_value
     assert configuration_dict[COMMANDS] == mock_commands_repository_as_dict.return_value
     assert configuration_dict[SOURCES] == mock_sources_repository_as_dict.return_value
